86816cf5eb4eb59995bb84c19e1555591ab9a32a
Julien Picaud <julienpicaud83@gmail.com>
2023-11-25 10:08:42 +0100

    perf mobile en cours

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -239,8 +239,8 @@
       if ($(this).hasClass("active-tag")) {
         return;
       }
-      $(".active-tag").removeClass("active active-tag");
-      $(this).addClass("active active-tag");
+      $(".active-tag").removeClass("active active-tag"); 
+      $(this).addClass("active active-tag"); // Ajout de la pseudo-classe active
 
       var tag = $(this).data("images-toggle");
 
